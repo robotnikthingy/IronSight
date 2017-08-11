43740961ec36173b76819bcb4805cd4cde6778d5
Yannick <yannicklamprecht@live.de>
2017-08-11 13:06:47 +0200

    Added startingpoint for split

--- Implementation/src/main/java/com/github/robotnikthingy/weaponsapi/WeaponsAPIPlugin.java
+++ Implementation/src/main/java/com/github/robotnikthingy/weaponsapi/WeaponsAPIPlugin.java
@@ -3,10 +3,17 @@ package com.github.robotnikthingy.weaponsapi;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
- * Created by ysl3000
+ * Created by Robotnik on 8/4/2017.
  */
-public class WeaponsAPIPlugin extends JavaPlugin{
+public class WeaponsAPIPlugin extends JavaPlugin {
 
 
+    private WeaponsAPIInterface weaponsAPIInterface;
 
+    @Override
+    public void onEnable() {
+        this.weaponsAPIInterface = new WeaponsAPIImplementation();
+        WeaponsAPI.api.setApi(weaponsAPIInterface);
+
+    }
 }
