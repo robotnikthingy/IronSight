98bbd7ed4c119a2eac26f8778e76bf2675abb424
Parker Hawke <hawkeboyz2@hotmail.com>
2017-09-10 18:33:40 -0400

    Improve MechanicParser to use the RegEx specified in AbstractMechanic

--- Implementation/src/main/java/com/github/robotnikthingy/weaponsapi/action/MechanicParser.java
+++ Implementation/src/main/java/com/github/robotnikthingy/weaponsapi/action/MechanicParser.java
@@ -1,29 +1,60 @@
 package com.github.robotnikthingy.weaponsapi.action;
 
+import java.util.function.BiFunction;
+import java.util.regex.Matcher;
+
 /**
  * A parser to parse actions and their parameters from configuration files
  * 
  * @author Robotnik
  * @since 8/9/2017
  */
-public class MechanicParser {
-
-
-    MechanicAction parseAction(String configLine){
-
-        //get all text before the parenthesis, which is the name of the mechanicAction
-        String actionName = configLine.split("\\(")[0];
-
-        //everything after first parenthesis
-        String parameterString = configLine.split("\\(")[1];
-        //remove the last parenthesis
-        parameterString.split(configLine.split("\\)")[0]);
-
-        //get all the parameters
-        String[] parameters = parameterString.split(",");
-
-        MechanicAction mechanicAction = new MechanicAction(actionName,parameters);
-
-        return mechanicAction;
-    }
+public final class MechanicParser {
+	
+	private MechanicParser() {}
+	
+	/**
+	 * Parse and return a MechanicAction from a provided String value
+	 * 
+	 * @param action the action string to parse
+	 * @return the resulting MechanicAction. null if unsuccessful parse
+	 * 
+	 * @author Parker Hawke - 2008Choco
+	 */
+	public static MechanicAction parseAction(String action) {
+		return parse(action, MechanicAction::new);
+	}
+	
+	/**
+	 * Parse and return a MechanicRequirement from a provided String value
+	 * 
+	 * @param requirement the requirement string to parse
+	 * @return the resulting MechanicRequirement. null if unsuccessful parse
+	 * 
+	 * @author Parker Hawke - 2008Choco
+	 */
+	public static MechanicRequirement parseRequirement(String requirement) {
+		return parse(requirement, MechanicRequirement::new);
+	}
+	
+	/**
+	 * Parse any variant of AbstractMechanic and create a new instance of it
+	 * 
+	 * @param toParse the value to parse
+	 * @param creation the instantiation function
+	 * 
+	 * @return the resulting abstract mechanic
+	 * 
+	 * @author Parker Hawke - 2008Choco
+	 */
+	private static <T extends AbstractMechanic> T parse(String toParse, BiFunction<String, String[], T> creation) {
+		Matcher matcher = AbstractMechanic.MECHANIC_PATTERN.matcher(toParse);
+		if (!matcher.find()) return null;
+		
+		String name = matcher.group(1);
+		String[] parameters = matcher.group(2).split("\\s*,\\s*");
+		
+		return creation.apply(name, parameters);
+	}
+	
 }
